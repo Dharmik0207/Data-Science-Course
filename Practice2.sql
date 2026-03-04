1ffcd385c30e7ed22bf8933750230e6cceacef37
Dharmik Mukeshbhai Gadhiya <dharmikgadhiya0207@gmail.com>
2026-03-04 16:06:39 +0100

    Add files via upload

---
+++ Practice2.sql
@@ -0,0 +1,7 @@
+
+												-- Joins In MySQL
+
+-- SELECT students.name, marks.subject, marks.score from students inner join marks on students.id=marks.student_id; -- inner Join 
+-- SELECT students.name, marks.subject, marks.score from students left join marks on students.id=marks.student_id; -- Left Join 
+--  SELECT students.name, marks.subject, marks.score from students right join marks on students.id=marks.student_id; -- Right Join 
+-- SELECT students.name, marks.subject, marks.score from students cross join marks; -- Cross Join 
